16a408d07c772a2e1294c27217653d97c4e8dcf5
denis-snitko <denissnitko@gmail.com>
2022-11-18 12:29:31 +0300

    feat: markup

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,10 @@
-
+const swiper = new Swiper(".card__promotion-swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
