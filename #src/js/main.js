afa796ccf3482345dd2d5183df99bb5529390fa5
denis-snitko <denissnitko@gmail.com>
2022-11-25 18:02:18 +0300

    feat: markup

--- #src/js/main.js
+++ #src/js/main.js
@@ -13,11 +13,15 @@ const swiper = new Swiper('.card__promotion-swiper', {
 
 var galleryThumbs = new Swiper('.card__slider--thumbs', {
   direction: 'vertical',
-  slidesPerView: 4,
-  slideToClickedSlide: false,
+  slidesPerView: 7,
+  // slideToClickedSlide: false,
   spaceBetween: 10,
   // loopedSlides: 50,
   loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next-th',
+    prevEl: '.swiper-button-prev-th',
+  },
 });
 
 var galleryMain = new Swiper('.card__slider--main', {
@@ -61,4 +65,44 @@ const openModal = () => {
   }
 };
 
+const activeTab = () => {
+  const EL_tabs = document.querySelectorAll('[name="card-info"]');
+
+  if (EL_tabs) {
+    EL_tabs.forEach((currentElement) => {
+      currentElement.addEventListener('click', () => {
+        document.querySelectorAll('.js-tabs-label').forEach((el) => el.classList.remove('active'));
+        document.querySelector(`[for="${currentElement.id}"]`).classList.add('active');
+      });
+    });
+  }
+};
+
+const popupTimeOpen = () => {
+  const EL_btnOpen = document.querySelectorAll('.js-popup-open');
+
+  if (EL_btnOpen) {
+    EL_btnOpen.forEach((currentElement) => {
+      currentElement.addEventListener('click', () => {
+        currentElement.classList.toggle('active');
+      });
+    });
+  }
+};
+
+const badgesHandler = () => {
+  const EL_hiddenBadges = document.querySelector('.js-badges__hidden');
+  const EL_showMoreBtn = document.querySelector('.js-show-more');
+
+  if (EL_hiddenBadges && EL_showMoreBtn) {
+    EL_showMoreBtn.addEventListener('click', () => {
+      EL_showMoreBtn.classList.toggle('active');
+      EL_hiddenBadges.classList.toggle('hidden');
+    });
+  }
+};
+
 openModal();
+activeTab();
+popupTimeOpen();
+badgesHandler();
